575592f06bbbe2e0b967d3fbfc465324fe4678c7
Anthony Rurup <tcrurup@gmail.com>
2019-06-19 04:42:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+  
+  
+  
+  post '/recipes/new' do
+    recipe = Recipe.create(params)
+    redirect "recipies/#{recipe.id}"
+  end
 
 end
